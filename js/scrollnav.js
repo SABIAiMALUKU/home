0de81af281eaf68bf2310a19801f4b1550c6ce5d
SABIAiMALUKU <145528378+SABIAiMALUKU@users.noreply.github.com>
2023-09-19 22:17:49 -0700

    Add files via upload

---
+++ js/scrollnav.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('main-navbar');
+    if (window.scrollY > 50) { 
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
